ede28b199d97c71f380ab79d6e1c062007ad0d4c
Jim May <jim_nastic@hotmail.com>
2015-01-15 19:48:41 +1100

    Share and quick scroll button events

--- js/custom.js
+++ js/custom.js
@@ -28,8 +28,17 @@ $(".navbar-nav li a[href^='#']").on('click', function(e) {
     }, 1000);
 });
 // --------------------------------------------------------
-//	Google Analytics signup submit
+//	Google Analytics Events
 // -------------------------------------------------------- 	
+// Try it Free quick scroll
+$('#left-social-link').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'Share', 1);
+});
+// Get Early Access Button click
 $('.untorch-submit-button').on('click', function() {
   ga('send', 'event', 'button', 'click', 'GetEarlyAccess', 1);
+});
+// Facebook Share
+$('.btn-submit').on('click', function() {
+  ga('send', 'event', 'button', 'click', 'QuickScroll', 1);
 });
